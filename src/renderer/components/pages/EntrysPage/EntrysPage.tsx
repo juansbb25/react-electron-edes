2d12a398f0ae02eedcb11c2cf47652a7084cb10b
juansbb25 <juansbb25@gmail.com>
2021-10-09 10:12:34 -0500

    Add datePicker

--- src/renderer/components/pages/EntrysPage/EntrysPage.tsx
+++ src/renderer/components/pages/EntrysPage/EntrysPage.tsx
@@ -1,11 +1,27 @@
 import InputsForm from "@components/atoms/InputsForm";
 import UILayout from "@components/layout/UILayout";
 import React from "react";
+import AdapterMoment from "@mui/lab/AdapterMoment";
+import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import DatePicker from "@mui/lab/DatePicker";
+import { StandardTextFieldProps, TextField } from "@material-ui/core";
 type EntrysPageProps = {};
 const EntrysPage: React.FC<EntrysPageProps> = () => {
+  const [value, setValue] = React.useState(null);
+
   return (
     <UILayout title="Ingresos">
-      <InputsForm items={[{ label: "ingresos", id: "ingresos" }]}></InputsForm>
+      <LocalizationProvider dateAdapter={AdapterMoment}>
+        <DatePicker
+          label="Basic example"
+          value={value}
+          onChange={(newValue) => {
+            setValue(newValue);
+          }}
+          // TODO add explicit type to params
+          renderInput={(params: any) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
     </UILayout>
   );
 };
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
+
 declare module "@material-ui/core/Button" {
   interface ButtonPropsColorOverrides {
     error: true;
