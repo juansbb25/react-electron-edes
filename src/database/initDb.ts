fca5d7e4de10a520ab76d2afa0f4a521732cf22d
Hugo Betancourt <hugo@dreamcode.io>
2021-10-11 12:25:48 -0500

    fix database initialization

--- src/database/initDb.ts
+++ src/database/initDb.ts
@@ -11,7 +11,7 @@ export const initDatabase = async (): Promise<EnhancedDb> => {
 
   await db.read();
   // Read data from JSON file, this will set db.data content
-  db.data ||= { presupuesto: [], presupuestoCode: [] };
+  db.data ||= { presupuestos: [], ingresos: [], gastos: [] };
   const chain = lodash.chain(db.data);
   db.chain = chain;
   return db;
